520319d52dd6819a700678e338c7798e866ad08e
reine-ishyanami <2402979195@qq.com>
2022-06-10 16:21:35 +0800

    修改JDK为8

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.reine.clock {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.reine.clock to javafx.fxml;
-    exports com.reine.clock;
-}
